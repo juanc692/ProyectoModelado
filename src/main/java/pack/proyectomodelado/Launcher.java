cdd0ed79f72622cce6879706688d9ade9c26d28c
juanc692 <picosalavarrietajuandavid50@gmail.com>
2026-02-20 12:14:47 -0500

    Actualizacion menuprincipal

--- src/main/java/pack/proyectomodelado/Launcher.java
+++ src/main/java/pack/proyectomodelado/Launcher.java
@@ -3,6 +3,7 @@ package pack.proyectomodelado;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,12 @@ public class Launcher extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+        stage.setTitle("Parchador");
+        Image img = new Image(getClass().getResource("/images/logo.jpg").toExternalForm());
+        stage.getIcons().add(img);
+
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
         stage.show();
     }
 }
--- src/main/java/pack/proyectomodelado/front/menuControlador.java
+++ src/main/java/pack/proyectomodelado/front/menuControlador.java
@@ -2,13 +2,23 @@ package pack.proyectomodelado.front;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class menuControlador {
+    public ImageView logo;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void initialize(){
+        Image img = new Image(getClass().getResource("/images/logo.jpg").toExternalForm());
+
+        logo.setImage(img);
+
     }
 }
