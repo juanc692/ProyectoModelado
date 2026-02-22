0bfb1c74732406dbc0f904741b1cdfafaa52b623
Santiago Rodriguez <erikrodriguezespitia@gmail.com>
2026-02-21 19:34:54 -0500

    update

--- src/main/java/pack/proyectomodelado/entidades/evento.java
+++ src/main/java/pack/proyectomodelado/entidades/evento.java
@@ -6,4 +6,43 @@ public class evento {
     private String nombre;
     private String categoria;
     private float precioEntrada;
+
+    public evento(String ciudad, String nombre, String categoria, float precioEntrada) {
+        this.ciudad = ciudad;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precioEntrada = precioEntrada;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public float getPrecioEntrada() {
+        return precioEntrada;
+    }
+
+    public void setPrecioEntrada(float precioEntrada) {
+        this.precioEntrada = precioEntrada;
+    }
 }
--- src/main/java/pack/proyectomodelado/front/interfazPrincipalControl.java
+++ src/main/java/pack/proyectomodelado/front/interfazPrincipalControl.java
@@ -2,12 +2,70 @@ package pack.proyectomodelado.front;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import pack.proyectomodelado.entidades.evento;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class interfazPrincipalControl {
 
     @FXML
     private Button btnCrearEvento;
+    @FXML
+    private AnchorPane anchorFormulario;
+    @FXML
+    private AnchorPane anchorPrincipal;
+    @FXML
+    private ComboBox comboCiudad;
+    @FXML
+    private TextField fieldNombre;
+    @FXML
+    private ComboBox comboCategoria;
+    @FXML
+    private TextField fieldEntrada;
+    @FXML
+    private Button btnCrear;
 
 
+    private ArrayList <evento> listaEventos = new ArrayList<>();
+
+    @FXML
+    public void initialize(){
+        btnCrearEvento.setOnAction(e->{
+            mostrarFormulario();
+        });
+        btnCrear.setOnAction(e->{
+            anadirEvento();
+        });
+
+    }
+
+    private void mostrarFormulario(){
+        anchorPrincipal.setVisible(false);
+        anchorPrincipal.setDisable(true);
+
+        anchorFormulario.setVisible(true);
+        anchorFormulario.setDisable(false);
+    }
+
+    private void ocultarFormulario(){
+        anchorPrincipal.setVisible(true);
+        anchorPrincipal.setDisable(false);
+
+        anchorFormulario.setVisible(false);
+        anchorFormulario.setDisable(true);
+    }
+
+    private void anadirEvento(){
+        if (comboCiudad != null || comboCategoria != null || fieldNombre.getText().isEmpty() || Float.valueOf(fieldEntrada.getText()) <= 0 ){
+            evento nuevo = new evento(comboCiudad.getValue().toString(),fieldNombre.getText(),comboCategoria.getValue().toString(),Float.valueOf(fieldEntrada.getText()));
+            System.out.println(nuevo.getPrecioEntrada()+" exito");
+            listaEventos.add(nuevo);
+            ocultarFormulario();
+        }
+    }
 
 }
